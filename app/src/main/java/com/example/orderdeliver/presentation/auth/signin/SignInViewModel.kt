cd8e5564fed807c894dd501031bae5946c0944e9
developer-mixa <mixail_081177@mail.ru>
2024-11-11 22:41:05 +0300

    feat: Added going back if user signed up

--- app/src/main/java/com/example/orderdeliver/presentation/auth/signin/SignInViewModel.kt
+++ app/src/main/java/com/example/orderdeliver/presentation/auth/signin/SignInViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.orderdeliver.domain.requests.SignInRequest
 import com.example.orderdeliver.domain.usecases.SignInUseCase
+import com.example.orderdeliver.presentation.plugins.plugins.NavigatorPlugin
 import com.example.orderdeliver.utils.showLog
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -12,12 +13,14 @@ import javax.inject.Inject
 @HiltViewModel
 class SignInViewModel @Inject constructor(
     private val signInUseCase: SignInUseCase,
+    private val navigator: NavigatorPlugin
 ): ViewModel() {
 
     fun signIn(username: String, password: String) = viewModelScope.launch {
         try {
             signInUseCase(SignInRequest(username, password))
-        }catch (e: Exception){
+            navigator.goBack()
+        }catch (e: Exception) {
             showLog(e.toString())
         }
     }
